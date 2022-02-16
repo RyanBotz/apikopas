2fe8d8d8a48a5bf7f1bc630d7c448239c5e79b58
RyanBotz <98251598+RyanBotz@users.noreply.github.com>
2022-02-16 17:57:13 +0700

    kopas

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var express = require('express'),
-    cors = require('cors'),
-    secure = require('ssl-express-www');
+//     cors = require('cors'),
+//     secure = require('ssl-express-www');
 const PORT = process.env.PORT || 8080 || 5000 || 3000
-var { color } = require('./lib/color.js')
+// var { color } = require('./lib/color.js')
 
 // var mainrouter = require('./routes/main'),
 //     apirouter = require('./routes/api')
 
-// var app = express()
+var app = express()
 // app.enable('trust proxy');
 // app.set("json spaces",2)
 // app.use(cors())
@@ -18,7 +18,7 @@ var { color } = require('./lib/color.js')
 // app.use('/api', apirouter)
 
 app.listen(PORT, () => {
-    console.log(color("Server running on port " + PORT,'green'))
+    console.log("Server running on port " + PORT)
 })
 
 module.exports = app
